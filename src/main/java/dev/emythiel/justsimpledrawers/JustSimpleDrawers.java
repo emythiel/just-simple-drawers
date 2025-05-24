c9b41ca282d41043aada03a56b35af391aa04087
emythiel <18901520+emythiel@users.noreply.github.com>
2025-05-24 19:43:08 +0200

    add initial drawer blocks and upgrade items
    they don't do anything yet though

--- src/main/java/dev/emythiel/justsimpledrawers/JustSimpleDrawers.java
+++ src/main/java/dev/emythiel/justsimpledrawers/JustSimpleDrawers.java
@@ -1,5 +1,8 @@
 package dev.emythiel.justsimpledrawers;
 
+import dev.emythiel.justsimpledrawers.block.ModBlocks;
+import dev.emythiel.justsimpledrawers.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -36,6 +39,9 @@ public class JustSimpleDrawers {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -49,7 +55,20 @@ public class JustSimpleDrawers {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.DRAWER_UPGRADE_BASE);
+            event.accept(ModItems.DRAWER_UPGRADE_T1);
+            event.accept(ModItems.DRAWER_UPGRADE_T2);
+            event.accept(ModItems.DRAWER_UPGRADE_T3);
+            event.accept(ModItems.DRAWER_UPGRADE_T4);
+        }
 
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.DRAWER_SINGLE);
+            event.accept(ModBlocks.DRAWER_DOUBLE);
+            event.accept(ModBlocks.DRAWER_QUAD);
+            event.accept(ModBlocks.DRAWER_COMPACTING);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
