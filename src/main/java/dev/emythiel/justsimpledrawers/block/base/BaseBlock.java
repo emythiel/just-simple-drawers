43222bc7c974fdbc871c6bf8bffa193ad03a4725
emythiel <18901520+emythiel@users.noreply.github.com>
2025-05-27 19:09:52 +0200

    Add BaseBlock class for directional placement

---
+++ src/main/java/dev/emythiel/justsimpledrawers/block/base/BaseBlock.java
@@ -0,0 +1,34 @@
+package dev.emythiel.justsimpledrawers.block.base;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import org.jetbrains.annotations.Nullable;
+
+public class BaseBlock extends HorizontalDirectionalBlock {
+    public static final MapCodec<BaseBlock> CODEC = simpleCodec(BaseBlock::new);
+
+    public BaseBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
+        return CODEC;
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING,context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+}
--- src/main/java/dev/emythiel/justsimpledrawers/block/base/ControllerBlock.java
+++ src/main/java/dev/emythiel/justsimpledrawers/block/base/ControllerBlock.java
@@ -4,11 +4,10 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 
-public class ControllerBlock extends Block {
+public class ControllerBlock extends BaseBlock {
     public ControllerBlock(Properties properties) {
         super(properties);
     }
